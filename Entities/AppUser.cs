1e74ed5fe03670e74daaea774db5d42f8f3a80ab
DESKTOP-SLOG6QV\thomp <louis.thompson@bepsystems.co.uk>
2021-09-03 14:44:32 +0100

    Add project files.

---
+++ Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //Protected can be inherited from this class, and any class that inherits from this class.
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+       
+    }
+}
