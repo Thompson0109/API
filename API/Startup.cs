74f21ea71867dd7ef9bf495dad1cbb83a9137c89
DESKTOP-SLOG6QV\thomp <louis.thompson@bepsystems.co.uk>
2021-09-22 10:26:51 +0100

    Added Angular to project and provided dummy data for DB.

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API
             _config = config;
         }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -31,11 +32,13 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Ordering of these methods is important.
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,6 +47,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 
             app.UseAuthorization();
 
